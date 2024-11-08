4bf7fde5af17c1ef39ad83d94d051813448f2b12
jdg489 <a67704287@gamil.com>
2024-11-08 20:40:53 +0900

    컨트롤 미디어쿼리 정상화 완료

--- KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/manager/controller/ManagerController.java
+++ KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/manager/controller/ManagerController.java
@@ -5,12 +5,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("manager/")
+@RequestMapping("/manager")
 public class ManagerController {
 	
 	@RequestMapping("notice")
-	public String mainPage(Model m) {
+	public String noticePage(Model m) {
 		m.addAttribute("pageName","managerPage");
 		return "manager/manager";
+	}
+	
+	@RequestMapping("certify")
+	public String certifyPage(Model m) {
+		m.addAttribute("pageName","certifyPage");
+		return "manager/certify";
+	}
+	
+	@RequestMapping("list")
+	public String listPage(Model m) {
+		m.addAttribute("pageName","listPage");
+		return "manager/list";
+	}
+	
+	@RequestMapping("report")
+	public String reportPage(Model m) {
+		m.addAttribute("pageName", "reportPage");
+		return "manager/report";
+	}
+	
+	@RequestMapping("user")
+	public String userPage(Model m) {
+		m.addAttribute("pageName","userPage");
+		return "manager/user";
 	}
 }
