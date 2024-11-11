d23ccf32e1e4f7f2e60e1f00d3187cd837cd7c91
tkdwns3269 <tkdwns3269@naver.com>
2024-11-11 18:47:01 +0900

    중간저장

--- KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/calendar/model/service/CalendarService.java
+++ KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/calendar/model/service/CalendarService.java
@@ -1,5 +1,19 @@
 package com.kh.T3B1.calendar.model.service;
 
-public class CalendarService {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.T3B1.calendar.model.dao.CalendarDao;
+import com.kh.T3B1.calendar.model.vo.Calendar;
+
+@Service
+public class CalendarService {
+	@Autowired(required = false)
+	private CalendarDao dao;
+	
+	public ArrayList<Calendar> calendarList(){
+		return dao.calendarList();
+	}
 }
