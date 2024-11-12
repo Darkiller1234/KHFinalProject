6c4ae58153c1ed79f72d865f35b5d02b0b263583
tkdwns3269 <tkdwns3269@naver.com>
2024-11-12 21:32:42 +0900

    롤백

--- KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/info/controller/InfoPageController.java
+++ KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/info/controller/InfoPageController.java
@@ -6,22 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InfoPageController {
-	
 
-	
-	//검색 페이지로 이동
-	  @RequestMapping("search")
-	    public String searchPage(Model m) {
+	// 검색 페이지로 이동
+	@RequestMapping("search")
+	public String searchPage(Model m) {
 
-	        return "infoPage/searchPage";  
-	  }
-	
+		return "infoPage/searchPage";
+	}
 
-	//정보창으로 이동
+	// 정보창으로 이동
 	@RequestMapping("info")
 	public String infoPage(Model m) {
 		m.addAttribute("pageName", "infoPage");
 		return "infoPage/infoPage";
 	}
-	
+
 }
