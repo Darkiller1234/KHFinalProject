3b520ee385bb51355ae49e2a31aa670920553834
Darkiller1234 <kyoung8292@naver.com>
2024-11-14 17:55:18 +0900

    no message

--- KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/community/communityDetailAjax.js
+++ KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/community/communityDetailAjax.js
@@ -10,3 +10,16 @@ function getWriterProfileImg(data, callback){
         }
     })
 }
+
+function getReplyList(data, callback) {
+    $.ajax({
+        url: "detail/replyListJson",
+        data: data,
+        success: function(res){
+            callback(res);
+        },
+        error: function(res){
+            console.log("댓글 가져오는 ajax 오류");
+        }
+    })
+}
