e8db819b4dbef62ff3f3df6ba14e404399aead55
Kim-dong-young <rlaehddud9502@naver.com>
2024-11-18 11:16:15 +0900

    수정 - db 댓글내용 컬럼 추가 및 멘토 좋아요 로직 수정

--- KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/studyroom/mentorDetail.js
+++ KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/studyroom/mentorDetail.js
@@ -1,8 +1,8 @@
-function initMentorDetail(contextPath){
-    initLikeButton(contextPath)
+function initMentorDetail(contextPath, isLogin){
+    initLikeButton(contextPath, isLogin)
 }
 
-function initLikeButton(contextPath){
+function initLikeButton(contextPath, isLogin){
     // 현재 페이지의 URL 주소
     const url = new URL(window.location.href);
     // URL의 파라미터값을 가진 객체
@@ -25,33 +25,39 @@ function initLikeButton(contextPath){
         likeTag.innerHTML = ""
         let likeTagImg = document.createElement("img")
 
-        // res.type = 좋아요를 이미 눌렀는지 여부
-        if(res.type == 'N'){ // 누른적이 없다면 => 누른 모양으로 변경
+        // res.type = 좋아요를 눌렀는지 여부
+        if(res.type == 'Y'){
             likeTag.className = "tag bgcolor4"
             likeTagImg.src = contextPath + "/resources/static/img/profile/full_heart.png"
-        } else { // 이미 눌렀었다면 => 누르지 않은 모양으로 변경
+        } else {
             likeTag.className = "tag bgcolor3"
             likeTagImg.src = contextPath + "/resources/static/img/profile/heart.png"
         }
-        
+
         likeTag.appendChild(likeTagImg)
         likeTag.innerHTML += res.likeCount
     }
 
+    // 페이지 첫 로딩 시 좋아요 정보 불러오기
+    ajaxInitLike(likeInfo, onLikeMentor);
+
+    // 좋아요 버튼 클릭 시 좋아요 처리
     let likeMentor = ajaxLikeMentor(likeInfo, onLikeMentor)
 
-    likeTag.onclick = () => {
-        if(timer){ // 아직 수행되지 않았다면 return
-            return;
+    if(isLogin == 'Y'){
+        likeTag.onclick = () => {
+            if(timer){ // 아직 수행되지 않았다면 return
+                return;
+            }
+    
+            likeMentor() // ajax 수행후 이벤트 제거
+            likeMentor = null;
+    
+            timer = setTimeout(()=>{ // 1초뒤 이벤트 다시 부여
+                likeMentor = ajaxLikeMentor(likeInfo, onLikeMentor)
+                timer = 0
+            },1000)
         }
-
-        likeMentor() // ajax 수행후 이벤트 제거
-        likeMentor = null;
-
-        timer = setTimeout(()=>{ // 1초뒤 이벤트 다시 부여
-            likeMentor = ajaxLikeMentor(likeInfo, onLikeMentor)
-            timer = 0
-        },1000)
     }
 }
 
@@ -68,4 +74,17 @@ function ajaxLikeMentor(likeInfo, callback){
             }
         })
     }
+}
+
+function ajaxInitLike(likeInfo, callback){
+    $.ajax({
+        url:'checkLike',
+        data:{
+            mentorNo : likeInfo.mentorNo
+        },
+        success: callback,
+        error: () => {
+            console.log('좋아요 초기화 실패')
+        }
+    })
 }
