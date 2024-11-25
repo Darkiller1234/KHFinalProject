fb1218a9c86cb61667d5aca6216550acb1cf84cf
Kim-dong-young <rlaehddud9502@naver.com>
2024-11-25 09:18:43 +0900

    main.java 충?돌

--- KHFinalCertiHub/T3B1/src/main/resources/docker/Main.java
+++ KHFinalCertiHub/T3B1/src/main/resources/docker/Main.java
@@ -4,10 +4,9 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 4; i++){
-System.out.println("Hello world!");
-}
-        System.out.println("Hello world!");
+while(true){
+        system.println(main)
     }
+}
 }
                             
