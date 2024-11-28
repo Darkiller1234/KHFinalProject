dea75fe4cc3b99dfb79f233483c5ff9e345e4f3f
Kim-dong-young <rlaehddud9502@naver.com>
2024-11-29 00:49:59 +0900

    추가 - gpt api에게 응답 받아오기

--- KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/chatbot/service/ChatbotServiceImpl.java
+++ KHFinalCertiHub/T3B1/src/main/java/com/kh/T3B1/chatbot/service/ChatbotServiceImpl.java
@@ -11,17 +11,28 @@ import java.net.URL;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.kh.T3B1.chatbot.model.dao.ChatbotDao;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
+@PropertySource("classpath:/config/config.properties")
 public class ChatbotServiceImpl implements ChatbotService{
+	
+	public ChatbotDao chatbotDao;
 
 	@Value("${spring.datasource.gptKey}")
 	private String GPT_API_KEY;
-	
+
 	@Override
 	public String getChat(HashMap<String, String> sendInfo) {
 		String url = "https://api.openai.com/v1/chat/completions";
@@ -37,7 +48,7 @@ public class ChatbotServiceImpl implements ChatbotService{
 			
 			JsonObject jsonObject = new JsonObject();
 			jsonObject.addProperty("model","gpt-4o-mini");
-			jsonObject.addProperty("temperature","0.7");
+			jsonObject.addProperty("temperature",0.7);
 			
 			JsonObject msg = new JsonObject();
 			msg.addProperty("role", "user");
@@ -48,27 +59,26 @@ public class ChatbotServiceImpl implements ChatbotService{
 			
 			jsonObject.add("messages", msgArray);
 			
+			urlConnection.setDoOutput(true); // OutputStream 사용 가능 설정
 			try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"))) {
 	            writer.write(jsonObject.toString());
 	            writer.flush();
 	        }
 			
-			BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-			
-			String line;
-			while((line = br.readLine()) != null) {
-				result += line;
-			}
-			
-			br.close();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            
+            br.close();
 			urlConnection.disconnect();
-			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		 
 		return result;
 	}
 	
