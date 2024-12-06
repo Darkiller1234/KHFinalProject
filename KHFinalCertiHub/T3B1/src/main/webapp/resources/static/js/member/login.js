e0674f2bfb68c6919b00491386ed4e2a5d931907
Darkiller1234 <kyoung8292@naver.com>
2024-12-06 14:03:05 +0900

    네이버로그인 정상화

--- KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/member/login.js
+++ KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/member/login.js
@@ -1,7 +1,7 @@
 function naverLogin(){
     const clientId = "8spRhaZdAbZLcdcd6e4Q";
     //리다이렉트URL을 utf-8로 인스코딩해서 저장
-    const redirectURL = encodeURIComponent("http://192.168.30.12:5600/T3B1/member/login/naver");
+    const redirectURL = encodeURIComponent("http://localhost:5600/T3B1/member/login/naver");
 
     const state = Math.random().toString(36).substring(2);
 
