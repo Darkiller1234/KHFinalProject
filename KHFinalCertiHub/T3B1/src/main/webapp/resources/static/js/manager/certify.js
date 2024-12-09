fc0dcd0cd6c7e0d99867bbee10b55068bc44316c
jdg489 <a67704287@gamil.com>
2024-12-09 10:20:02 +0900

    자격증 수정

--- KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/manager/certify.js
+++ KHFinalCertiHub/T3B1/src/main/webapp/resources/static/js/manager/certify.js
@@ -1,43 +1,43 @@
 function initCertifyPage(contextPath) {
-    // initCertifyBoard(contextPath);
+    initCertifyBoard(contextPath);
     initCertifyPageBar(contextPath);
 }
 
-// function initCertifyBoard(contextPath) {
-//     const boardList = document.querySelector('.board-certify');
+function initCertifyBoard(contextPath) {
+    const boardList = document.querySelector('.board-certify');
 
-//     let data = {
-//         url: contextPath + "/manager/certify?no=",
-//         titleIndex : 2,
-//         header : [
-//             "NO.",
-//             "제목",
-//             "등록일",
-//             "수락하기",
-//             "삭제"
-//         ],
-//         boardList : [
-//             [
-//                 "1",
-//                 "1",
-//                 "자격증 인증합니다.",
-//                 "2023-10-5",
-//                 "수락하기",
-//                 "<button>삭제</button>"
-//             ],
-//             [   
-//                 "1",
-//                 "1",
-//                 "자격증 인증합니다.",
-//                 "2023-10-5",
-//                 "수락하기",
-//                 "<button>삭제</button>"
-//             ]
-//         ]
-//     }
+    let data = {
+        url: contextPath + "/manager/certify?no=",
+        titleIndex : 2,
+        header : [
+            "NO.",
+            "제목",
+            "등록일",
+            "수락하기",
+            "삭제"
+        ],
+        boardList : [
+            [
+                "1",
+                "1",
+                "자격증 인증합니다.",
+                "2023-10-5",
+                "수락하기",
+                "<button>삭제</button>"
+            ],
+            [   
+                "1",
+                "1",
+                "자격증 인증합니다.",
+                "2023-10-5",
+                "수락하기",
+                "<button>삭제</button>"
+            ]
+        ]
+    }
 
-//     createList(boardList, data)
-// }
+    createList(boardList, data)
+}
 
 function initCertifyPageBar(contextPath) {
     const pagingBar = document.querySelector('.certify-bar');
@@ -52,5 +52,5 @@ function initCertifyPageBar(contextPath) {
             contextPath + '/resources/static/img/button/arrow_right.png'
         ]
     }
-    // createPageBar(pagingBar, data)
+    createPageBar(pagingBar, data)
 }
